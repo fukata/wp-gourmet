7b7dd79317ea7668709b93a78db755197a608ff1
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2012-09-07 22:21:45 +0900

    change tab to spaces

--- lib/GourmetAdminSettingEvent.php
+++ lib/GourmetAdminSettingEvent.php
@@ -7,42 +7,42 @@ class GourmetAdminSettingEvent extends Gourmet {
         add_action('whitelist_options', array($this, 'addWhitelistOptions'));
     }
 
-	public function addMenu() {
-		$page = add_options_page(self::NAME.' Options', self::NAME, 'manage_options', __FILE__, array($this, 'generateOptionForm'));
-	}
-
-    public function addWhitelistOptions() {
-		$whitelist_options['wpgroumet'] = array(
-            $this->getKey('api_key'),
-        );
-		return $whitelist_options;
+    public function addMenu() {
+        $page = add_options_page(self::NAME.' Options', self::NAME, 'manage_options', __FILE__, array($this, 'generateOptionForm'));
     }
 
-	public function generateOptionForm() {
+    public function addWhitelistOptions() {
+        $whitelist_options['wpgroumet'] = array(
+            $this->getKey('api_key'),
+        );
+        return $whitelist_options;
+    }
+
+    public function generateOptionForm() {
 
 ?>
 
 <div class="wrap">
-	<h2><?php echo self::NAME ?></h2>
-	<form method="post" action="options.php">
-		<?php wp_nonce_field('wpgroumet-options'); ?>
-		<input type="hidden" name="action" value="update" />
-		<input type="hidden" name="option_page" value="wpgroumet" />
-		<h3><?php echo __('Settings', self::TEXT_DOMAIN) ?></h3>
-		<table class="form-table">
-			<tr valign="top">
-				<th scope="row">
-					<p><?php echo __('API KEY', self::TEXT_DOMAIN) ?></p>
-				</th>
-				<td>
-					<p><input type="text" name="<?php echo $this->getKey('api_key') ?>" value="<?php echo $this->getApiKey() ?>" size="70" /></p>
-				</td>
-			</tr>
+    <h2><?php echo self::NAME ?></h2>
+    <form method="post" action="options.php">
+        <?php wp_nonce_field('wpgroumet-options'); ?>
+        <input type="hidden" name="action" value="update" />
+        <input type="hidden" name="option_page" value="wpgroumet" />
+        <h3><?php echo __('Settings', self::TEXT_DOMAIN) ?></h3>
+        <table class="form-table">
+            <tr valign="top">
+                <th scope="row">
+                    <p><?php echo __('API KEY', self::TEXT_DOMAIN) ?></p>
+                </th>
+                <td>
+                    <p><input type="text" name="<?php echo $this->getKey('api_key') ?>" value="<?php echo $this->getApiKey() ?>" size="70" /></p>
+                </td>
+            </tr>
         </table>
 
-		<p class="submit">
-			<input type="submit" class="button-primary" value="<?php echo __('Save Changes', self::TEXT_DOMAIN) ?>" />
-		</p>
+        <p class="submit">
+            <input type="submit" class="button-primary" value="<?php echo __('Save Changes', self::TEXT_DOMAIN) ?>" />
+        </p>
     </form>
 </div>
 
